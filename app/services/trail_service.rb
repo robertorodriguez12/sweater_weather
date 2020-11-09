3bf3de7395dd35428016f648749bf72ca3c6f176
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-09 08:52:00 -0700

    Complete trail service

--- app/services/trail_service.rb
+++ app/services/trail_service.rb
@@ -1,6 +1,11 @@
 class TrailService
     def self.find_trail(data)
-        
+        response = conn.get('/data/get-trails') do |faraday|
+            faraday.params['lat'] = data[0]
+            faraday.params['lon'] = data[1]
+            faraday.params['key'] = ENV['TRAILS_API_Key']
+        end
+        JSON.parse(response.body, symbolize_names: true)
     end
     private
 
