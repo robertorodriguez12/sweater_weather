3eaa844e19081c00322132dd38dfa68b85b8cc15
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-09 09:17:53 -0700

    Adjust trail poro to begin process of finding distance to a trail

--- app/poro/trail.rb
+++ app/poro/trail.rb
@@ -1,11 +1,22 @@
 class Trail
+    
+    attr_reader :name,
+                :summary,
+                :difficulty, 
+                :location, 
+                :distance_to_trail
+
     def initialize(data, coord)
-        binding.pry
-        @name = data[:name]
-        @summary = data[:summary]
-        @difficulty = data[:difficulty]
-        @location = data[:location]
+        @name = data[:trails].first[:name]
+        @summary = data[:trails].first[:summary]
+        @difficulty = data[:trails].first[:difficulty]
+        @location = data[:trails].first[:location]
+        @start_point = coord
         @distance_to_trail = distance_to_trail
+    end
+
+    def distance_to_trail
+        a = MapFacade.fetch_distance(@location, @start_point)
     end
     
 end
