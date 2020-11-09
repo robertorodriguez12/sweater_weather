f719d0450ef06825b3a83aadc95b5017f518af36
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-09 08:50:59 -0700

    Update methods to use correct data

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::TrailsController < ApplicationController
     def index
         location = params[:location]
         coord = MapFacade.fetch_lat_long(location)
-        forecast = WeatherFacade.fetch_weather(lat_long)
-        trail = TrailFacade.find_trail(lat_long)
-        binding.pry
+        forecast = WeatherFacade.fetch_weather(coord)
+        trail = TrailFacade.find_trail(coord)
     end
 end 
