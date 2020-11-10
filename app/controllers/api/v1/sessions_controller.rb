9bc20fb38e7ae9f985c73d265e0ab13c76b176e3
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-09 18:28:58 -0700

    passed successful login for a user in the create method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        
+        @user = User.find_by(email: session_params[:email])
+        if @user && @user.authenticate(session_params[:password])
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user), status: 200
+        end
+    end
+
+    private
+
+    def session_params
+        params.permit(:email, :password)
     end
 end
