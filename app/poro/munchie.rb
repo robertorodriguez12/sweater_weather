c835888c5db2d8f5bfbc5ceafc705e229ae96aa2
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-10 10:16:28 -0700

    Adjust naming of methods to fit formatting

--- app/poro/munchie.rb
+++ app/poro/munchie.rb
@@ -1,14 +1,14 @@
 class Munchie
-    attr_reader :destination, :formatted_time, :forecast, :restaurant
+    attr_reader :destination_city, :travel_time, :forecast, :restaurant
 
     def initialize(yelp, travel_time, weather, destination)
         @yelp = yelp
         @travel_time = travel_time
         @weather = weather
-        @destination = destination.capitalize
+        @destination_city = destination.capitalize
     end
     
-    def formatted_time
+    def travel_time
         hour = @travel_time.slice(1, 1)
         minute = @travel_time.slice(3, 2)
         "#{hour} hours #{minute} min"
