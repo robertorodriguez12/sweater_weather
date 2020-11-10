6a62f2b4f8080df05b3f8154cbc08d66cd5690b8
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-09 18:18:12 -0700

    create user login test

---
+++ spec/api/v1/user/user_login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v1/users' do
+  it 'returing user gets api key in response' do
+    user = create(:user, email: 'whatever@example.com', password: "password" )
+
+    post "/api/v1/sessions?email=#{user.email}&password=#{user.password}"
+
+    expect(response).to be_successful
+    
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to have_key(:data)
+    expect(results[:data]).to have_key(:attributes)
+    expect(results[:data][:attributes]).to have_key(:api_key)
+    expect(results[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+end
