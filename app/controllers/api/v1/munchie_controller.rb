3c4fe4fd4a256f808e4fb003ebeca67cc83dd105
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-10 10:16:07 -0700

    Created munchie serializer and rendered in controller

--- app/controllers/api/v1/munchie_controller.rb
+++ app/controllers/api/v1/munchie_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MunchieController < ApplicationController
     def find
         munchie = MunchieFacade.find_restaurant(params)
+        render json: MunchieSerializer.new(munchie)
     end
 end 
