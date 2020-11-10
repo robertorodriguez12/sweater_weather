aaa83cdaacfde17b29914bcb4671b15e1d4ea1d5
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-10 15:23:37 -0700

    Add road trip endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get '/backgrounds', to: 'image#show'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/road_trip', to: 'road_trip#create'
     end
   end
 end
