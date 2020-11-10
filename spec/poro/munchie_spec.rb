e066146c9ba18edde3cb107aa5d073c7f8a28b45
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-10 11:47:44 -0700

    Finishied munchie poro test and added data to fixtures

--- spec/poro/munchie_spec.rb
+++ spec/poro/munchie_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Munchie do
     it 'can create a Munchie object' do
+        unparsed_yelp = File.read('spec/fixtures/yelp_data.json')
+        yelp = JSON.parse(unparsed_yelp, symbolize_names: true)
+        unparsed_weather = File.read('spec/fixtures/current_weather.json')
+        weather = JSON.parse(unparsed_weather, symbolize_names: true)
+        travel_time = File.read('spec/fixtures/travel_time.txt')
+        destination = "Denver, CO"
         
+        munchie = Munchie.new(yelp, travel_time, weather, destination)
+        
+
+        expect(munchie).to be_a(Munchie)
+        expect(munchie.destination_city).to be_a(String)
+        expect(munchie.travel_time).to be_a(String)
+        expect(munchie.forecast).to be_a(Hash)
+        expect(munchie.restaurant).to be_a(Hash)
     end
 end
