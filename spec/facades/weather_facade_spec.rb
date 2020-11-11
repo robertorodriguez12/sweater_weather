01852d549d0e4a9139c030a1d42224a0970faf75
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-10 17:42:58 -0700

    Finish weather facade test

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe WeatherFacade do
         expect(response.daily_weather).to be_an(Array)
         expect(response.hourly_weather).to be_an(Array)
     end
+
+    it 'can fetch weather for a location at estimated time of arrival' do
+        destination = [39.738453, -104.984853]
+        time = 1605160800
+
+        response = WeatherFacade.weather_at_arrival(destination, time)
+        expect(response).to be_a(Hash)
+        expect(response).to have_key(:temp)
+    end
 end
